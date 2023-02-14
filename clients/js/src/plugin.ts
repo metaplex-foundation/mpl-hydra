75aed7c5ea4918d7c0aa4f6f5ed77980636e073f
Loris Leiva <loris.leiva@gmail.com>
2023-02-14 20:08:16 +0000

    Update dependencies

--- clients/js/src/plugin.ts
+++ clients/js/src/plugin.ts
@@ -1,5 +1,5 @@
 import { UmiPlugin } from '@metaplex-foundation/umi-core';
-import { mplEssentials } from '@lorisleiva/mpl-essentials';
+import { mplEssentials } from '@metaplex-foundation/mpl-essentials';
 import { getMplHydraProgram } from './generated';
 
 export const mplHydra = (): UmiPlugin => ({
